19d7120d40bdfc0beb65f8fb151bbffcadfdfe1e
PRATANGGA <112964935+PRATANGGA@users.noreply.github.com>
2022-11-13 19:39:11 +0700

    Add files via upload

---
+++ Belajar javascript/js.js
@@ -0,0 +1,33 @@
+// alert('halo su');
+//prompt('masukan buwung:');
+
+//var nama = prompt('masukan nama:');
+//alert(nama);
+
+//confirm('kamu sangar?');
+
+//var tes = confirm('kamu sangar?');
+//alert(tes);
+
+//var tes = confirm('kamu sangar?');
+//if( tes === true){
+    //alert('user menekan ok');
+//}else{
+    //alert('user menekan kontl');
+//}
+
+//alert('selamat datang');
+//var lagi = true;
+
+//while( lagi === true){
+    //var nama = prompt('masukan nama:');
+    //alert('halo' + nama);
+
+    //lagi = confirm('coba ndes?');
+//}
+//alert('suwun');
+var nilaiAwal = 1;
+while(nilaiAwal <= 5){
+    console.log('Hello su!' + nilaiAwal + 'x');
+    nilaiAwal++;
+}
